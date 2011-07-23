bde8ea0ab8d32a6f421b72d2d87793c0eed2a6f7
yowasou <yowasou@yowasou-VirtualBox.(none)>
2011-07-23 15:28:35 +0900

    bgmstreamer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bgmstreamer::Application.config.secret_token = '2cd81c557b3c15311c87991b916e1dc035eba95485ceb1007f0de53299a8ad6f2cbacc95248a2a37408048456b472195345760d890abfa3b3042e79518038cf7'
